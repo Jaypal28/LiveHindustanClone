b4c0478971d523de102f59e74deba11b5f362067
Jaypal28 <chaudharyjaypal28@gmail.com>
2025-11-29 14:28:54 +0530

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // IMPORTANT: Required for GitHub Pages
+  output: "export",
+
+  // Since Next.js cannot optimize images in static export
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
     ],
   },
+
+  // Your GitHub repo name
+  basePath: "/LiveHindustanClone",
+  assetPrefix: "/LiveHindustanClone/",
 };
 
 export default nextConfig;
